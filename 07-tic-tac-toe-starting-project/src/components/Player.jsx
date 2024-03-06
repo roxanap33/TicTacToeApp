a2c386729f63c83e081188f9f4be2d9dc01e37fe
roxanap33 <roxana.ciocian@principal33.com>
2024-03-06 11:43:43 +0200

    made user input editable

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+  }
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required value={name}></input>
+          <input
+            type="text"
+            onChange={handleNameChange}
+            value={playerName}
+            required
+          ></input>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
