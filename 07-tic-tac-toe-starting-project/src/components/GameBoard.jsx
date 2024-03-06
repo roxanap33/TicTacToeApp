34cb29686c42f40f7ba2e506d8fb2f250b703f94
roxanap33 <roxana.ciocian@principal33.com>
2024-03-06 15:36:43 +0200

    84: Deleted comments on GameBoard

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -13,19 +13,6 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
     gameBoard[row][col] = player;
   }
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((previousGameBoard) => {
-  //       const updatedBoard = [
-  //         ...previousGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedBoard;
-  //     });
-
-  //     onSelectSquare();
-  //   }
 
   return (
     <ol id="game-board">
